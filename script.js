cbee6f12aced5c9067da4a0a75dde6ccc42b96d4
Destiny <dchildress4@murraystate.edu>
2026-02-19 14:54:04 -0600

    Update script.js

--- script.js
+++ script.js
@@ -1,15 +1,24 @@
 const button = document.getElementById('clickBtn');
 const image = document.getElementById('myImage');
-const images = ['Baby.jpg', 'Toddler.jpg', 'Kid.jpg', 'Pre-teen.jpg', 'High School.jpg'];
+const mouseoverDiv = document.getElementById('mouseoverImage');
+const images = [
+    { src: 'Baby.jpg', age: '1 years old' },
+    { src: 'Toddler.jpg', age: '4 years old' },
+    { src: 'Kid.jpg', age: '7 years old' },
+    { src: 'Pre-teen.jpg', age: '10 years old' },
+    { src: 'High School.jpg', age: '17 years old' }];
 let currIndex = 0;
 
-image.src = images[currIndex];
+image.src = images[currIndex].src;
 
 button.addEventListener('click', function() {
     currIndex = (currIndex + 1) % images.length;
-    image.src = images[currIndex];
+    image.src = images[currIndex].src;
     customFunction();
+});
 
+mouseoverDiv.addEventListener('mouseover', () => {
+  alert(`I was ${images[currIndex].age} in this picture`);
 });
 
 function customFunction() {
